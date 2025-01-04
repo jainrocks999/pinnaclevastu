3cc67e6da75c05cf9bb6717af8dbff6a513383f9
raju barde <rajub.forbearpro@gmail.com>
2025-01-04 13:55:40 +0530

    chage loading condition in paymentInfo screen

--- src/Screen/Main/paymentInformation/index.js
+++ src/Screen/Main/paymentInformation/index.js
@@ -16,107 +16,118 @@ import UserAddress from '../../../Component/userAddress/userAddress';
 import CourseInfoCard from '../../../Component/CourseInfoCard/CourseInfoCard';
 import Toast from 'react-native-simple-toast';
 import {RadioButton} from 'react-native-paper';
-import { useIsFocused, useNavigation } from '@react-navigation/native';
+import {useIsFocused, useNavigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import constants from '../../../Redux/constant/constants';
 import axios from 'axios';
 import Loader from '../../../Component/Loader';
-import { shipmethod } from '../../../Redux/Slice/orderSclice';
-import { useDispatch, useSelector } from 'react-redux';
-import { clearcartdata } from '../../../Redux/Slice/CartSlice';
+import {shipmethod} from '../../../Redux/Slice/orderSclice';
+import {useDispatch, useSelector} from 'react-redux';
+import {clearcartdata} from '../../../Redux/Slice/CartSlice';
 
 const ResidentalScreen = ({route}) => {
   const nav = route.params;
-const navigation=useNavigation();
-const isLoading = useSelector(state => state?.order?.loading);
-const ship = useSelector(state => state?.order?.shipm?.shipping_method);
-console.log('fkjfgjkfgkf ,fifgigi',ship);
-const dispatch=useDispatch();
+  const navigation = useNavigation();
+  const isLoading = useSelector(state => state?.order?.loading);
+  const ship = useSelector(state => state?.order?.shipm?.shipping_method);
+  console.log('fkjfgjkfgkf ,fifgigi', ship);
+  const dispatch = useDispatch();
   const [radioActive, setRadioActive] = useState('');
-  const [loading1,setLoading] =useState(false);
+  const [loading1, setLoading] = useState(false);
   const [userType, setUserType] = useState('');
-  const [totals, setTotals] = useState({ totalTaxAmount: '', totalAmount: '',totalPriceOnly:'' });
-  const amount = (isNaN(parseFloat(totals?.totalAmount)) ? 0 : parseFloat(totals?.totalAmount)) + (parseFloat(ship?.price ?? '0'));
+  const [totals, setTotals] = useState({
+    totalTaxAmount: '',
+    totalAmount: '',
+    totalPriceOnly: '',
+  });
+  const amount =
+    (isNaN(parseFloat(totals?.totalAmount))
+      ? 0
+      : parseFloat(totals?.totalAmount)) + parseFloat(ship?.price ?? '0');
 
- 
-  
- 
   const getItems = async () => {
     const token = await AsyncStorage.getItem('Token');
     const userid = await AsyncStorage.getItem('user_id');
 
-  
     dispatch(
-    await  shipmethod({
+      await shipmethod({
         url: 'fetch-shipment-method',
         token: token,
         user_id: userid,
       }),
     );
   };
-const calculateTotals = (productList, userType) => {
-  let totalTaxAmount = 0;
-  let totalAmount = 0;
-  let totalPriceOnly = 0;
+  const calculateTotals = (productList, userType) => {
+    let totalTaxAmount = 0;
+    let totalAmount = 0;
+    let totalPriceOnly = 0;
 
-  productList.forEach((product) => {
-    const { sale_price, student_price, franchise_price, price, qty, option ,tax_amount} = product;
-    const taxRate = option.taxRate || 0;
+    productList.forEach(product => {
+      const {
+        sale_price,
+        student_price,
+        franchise_price,
+        price,
+        qty,
+        option,
+        tax_amount,
+      } = product;
+      const taxRate = option.taxRate || 0;
 
-    const selectedPrice =
-      userType === 'customers' && sale_price
-        ? sale_price
-        : userType === 'student' && student_price
-        ? student_price
-        : userType === 'franchise' && franchise_price
-        ? franchise_price
-        : price;
+      const selectedPrice =
+        userType === 'customers' && sale_price
+          ? sale_price
+          : userType === 'student' && student_price
+          ? student_price
+          : userType === 'franchise' && franchise_price
+          ? franchise_price
+          : price;
 
-    const itemPrice = selectedPrice * qty;
-    const taxAmount = parseFloat(tax_amount);
-    const totalProductAmount = itemPrice + taxAmount;
+      const itemPrice = selectedPrice * qty;
+      const taxAmount = parseFloat(tax_amount);
+      const totalProductAmount = itemPrice + taxAmount;
 
-    totalTaxAmount += taxAmount;
-    totalAmount += totalProductAmount;
-    totalPriceOnly += itemPrice;
-  });
+      totalTaxAmount += taxAmount;
+      totalAmount += totalProductAmount;
+      totalPriceOnly += itemPrice;
+    });
 
-  return {
-    totalTaxAmount: totalTaxAmount.toFixed(2),
-    totalAmount: totalAmount.toFixed(2),
-    totalPriceOnly: totalPriceOnly.toFixed(2),
-  };
-};
-
-// Fetch user type and calculate totals
-useEffect(() => {
-  setTotals('');
-  
-  const usercheck = async () => {
-    const userStatus = await AsyncStorage.getItem('user_data');
-    const userData = JSON.parse(userStatus);
-    setUserType(userData?.user_type || ''); // Handle null/undefined user type
+    return {
+      totalTaxAmount: totalTaxAmount.toFixed(2),
+      totalAmount: totalAmount.toFixed(2),
+      totalPriceOnly: totalPriceOnly.toFixed(2),
+    };
   };
 
-  usercheck();
-}, []);
+  // Fetch user type and calculate totals
+  useEffect(() => {
+    setTotals('');
 
-useEffect(() => {
-  getItems();
-  if (userType && nav?.data?.item?.length > 0) {
-    const calculatedTotals = calculateTotals(nav?.data?.item, userType);
-    
-    setTotals(calculatedTotals);
-  }
-}, [nav?.data?.item, userType]);
-  
+    const usercheck = async () => {
+      const userStatus = await AsyncStorage.getItem('user_data');
+      const userData = JSON.parse(userStatus);
+      setUserType(userData?.user_type || ''); // Handle null/undefined user type
+    };
+
+    usercheck();
+  }, []);
+
+  useEffect(() => {
+    getItems();
+    if (userType && nav?.data?.item?.length > 0) {
+      const calculatedTotals = calculateTotals(nav?.data?.item, userType);
+
+      setTotals(calculatedTotals);
+    }
+  }, [nav?.data?.item, userType]);
+
   const data = {
-    shipping_option: ship?.shipping_method_id??'1',
+    shipping_option: ship?.shipping_method_id ?? '1',
     shipping_method: 'default',
     // shipping_method: ship?.name  ?? 'defalut',
-    shipping_amount: ship?.price??'',
-    shipping_type:"",
-    is_available_shipping:'1',
+    shipping_amount: ship?.price ?? '',
+    shipping_type: '',
+    is_available_shipping: '1',
     transaction_id: '79896585966',
     coupon_code: '',
     tax_amount: totals?.totalTaxAmount,
@@ -126,20 +137,20 @@ useEffect(() => {
     note: 'order note testing',
     customer_address_id: nav?.adress?.id ?? '',
     address: {
-      address_id:  nav?.adress?.id ?? '',
+      address_id: nav?.adress?.id ?? '',
       name: nav?.adress?.name ?? '',
       email: nav?.adress?.email ?? '',
-      phone:  nav?.adress?.phone ?? '',
-      country:  nav?.adress?.country ?? '',
-      state:  nav?.adress?.state ?? '',
+      phone: nav?.adress?.phone ?? '',
+      country: nav?.adress?.country ?? '',
+      state: nav?.adress?.state ?? '',
       city: nav?.adress?.city ?? '',
-      address:  nav?.adress?.address ?? '',
+      address: nav?.adress?.address ?? '',
     },
     amount: amount,
     currency: 'IND',
-    customer_id:  nav?.adress?.customer_id,
+    customer_id: nav?.adress?.customer_id,
     customer_type: 'Botble\\Ecommerce\\Models\\Customer',
-    payment_method: radioActive  ? 'cod' : '',
+    payment_method: radioActive ? 'cod' : '',
     payment_status: 'pending',
     description: 'testing perpose',
     tax_information: {
@@ -149,20 +160,19 @@ useEffect(() => {
       company_email: '',
     },
     user_id: nav?.adress?.customer_id,
-    rowid:nav?.data?.item?.[0]?.rowid ,
-     billing_status: nav?.adress?.billing_status??'',
-     billingAddress: {
+    rowid: nav?.data?.item?.[0]?.rowid,
+    billing_status: nav?.adress?.billing_status ?? '',
+    billingAddress: {
       // address_id:  nav?.adress?.id ?? '',
       name: nav?.adress?.billing_address?.name ?? '',
       email: nav?.adress?.billing_address?.email ?? '',
-      phone:  nav?.adress?.billing_address?.phone ?? '',
-      country:  nav?.adress?.billing_address?.country ?? '',
-      state:  nav?.adress?.billing_address?.state ?? '',
-      city:nav?.adress?.billing_address?.city ?? '',
-      address:  nav?.adress?.billing_address?.address ?? '',
+      phone: nav?.adress?.billing_address?.phone ?? '',
+      country: nav?.adress?.billing_address?.country ?? '',
+      state: nav?.adress?.billing_address?.state ?? '',
+      city: nav?.adress?.billing_address?.city ?? '',
+      address: nav?.adress?.billing_address?.address ?? '',
     },
   };
-
 
   const createbyord = async () => {
     try {
@@ -184,15 +194,17 @@ useEffect(() => {
       };
 
       const response = await axios.request(config);
-      console.log('create order to response ',response.data);
-      
+      console.log('create order to response ', response.data);
+
       if (response.data.status == 200) {
         setLoading(false);
         Toast.show(response.data.msg);
         dispatch(clearcartdata());
-        navigation.navigate('Thankyou',{order:response?.data,data:'Remedies'});
-      }
-      else{
+        navigation.navigate('Thankyou', {
+          order: response?.data,
+          data: 'Remedies',
+        });
+      } else {
         setLoading(false);
         Toast.show(response.data.msg);
       }
@@ -210,91 +222,72 @@ useEffect(() => {
     }
   };
 
-
-
-
-
-
-
-
-
-
-
-  const renderItem = ({ item, index }) => {
+  const renderItem = ({item, index}) => {
     const isLastItem = index === nav?.data?.item.length - 1; // Dynamically check if it's the last item
     const isGSTRow = item.id === '2'; // Example: Use dynamic checks for specific rows
-  
+
     return (
       <View
         style={[
           styles.card45,
-         styles.borderBottom, // Apply border unless it's the last item
+          styles.borderBottom, // Apply border unless it's the last item
           {
             paddingTop: 0,
             paddingBottom: isGSTRow ? 5 : 0, // Apply padding dynamically based on item condition
           },
-        ]}
-      >
+        ]}>
         <Text style={[item.isBold ? styles.third3 : styles.third1]}>
           {item.name}
         </Text>
-        <Text style={[styles.third2, item.isBold && { fontWeight: 'bold' }]}>
-
-
-        ₹  {
-                userType === 'customers' && item?.sale_price
-                  ? item?.sale_price
-                  : userType === 'student' && item?.student_price
-                  ? item?.student_price
-                  : userType === 'franchise' && item?.franchise_price
-                  ? item?.franchise_price
-                  : item?.price /* Default case when userType is null or undefined */
-              }
-
-       
-        {/* {item.price?.toFixed(2)} */}
+        <Text style={[styles.third2, item.isBold && {fontWeight: 'bold'}]}>
+          ₹{' '}
+          {
+            userType === 'customers' && item?.sale_price
+              ? item?.sale_price
+              : userType === 'student' && item?.student_price
+              ? item?.student_price
+              : userType === 'franchise' && item?.franchise_price
+              ? item?.franchise_price
+              : item?.price /* Default case when userType is null or undefined */
+          }
+          {/* {item.price?.toFixed(2)} */}
         </Text>
       </View>
     );
   };
-  
-
-
 
   return (
     <View style={styles.container}>
       {/* Header */}
       <View style={styles.header}>
-      <TouchableOpacity
-            onPress={() => navigation.goBack()}
-            
-            hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
-            >
-            <Image
-              style={styles.backBtn}
-              source={require('../../../assets/drawer/Back1.png')}
-            />
-          </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          hitSlop={{top: 10, bottom: 10, left: 10, right: 10}}>
+          <Image
+            style={styles.backBtn}
+            source={require('../../../assets/drawer/Back1.png')}
+          />
+        </TouchableOpacity>
 
         <View style={styles.headerview}>
           <Text style={styles.logoText}>Payment Information</Text>
         </View>
       </View>
-{loading1 || isLoading?<Loader/>:null}
+      {loading1 ? <Loader /> : null}
       <ScrollView contentContainerStyle={styles.servicesContainer}>
         {nav?.data1 === 'Remedies' && (
           <View>
             <UserAddress data={nav?.adress} />
           </View>
         )}
-       
-<View style={styles.cardContainer2}>
-<FlatList
-    data={nav?.data?.item}
-    keyExtractor={(item) => item.id?.toString()} // Ensure id is a string
-    renderItem={renderItem}
-  />
-    <View
+
+        <View style={styles.cardContainer2}>
+          <FlatList
+            data={nav?.data?.item}
+            keyExtractor={item => item.id?.toString()} // Ensure id is a string
+            renderItem={renderItem}
+          />
+          <View
             style={[
               styles.card45,
               styles.borderBottom,
@@ -303,7 +296,7 @@ useEffect(() => {
             <Text style={styles.third2}>{'Shipping Charges'}</Text>
             <Text style={[styles.third2]}>₹ {ship?.price}</Text>
           </View>
-      <View
+          <View
             style={[
               styles.card45,
               styles.borderBottom,
@@ -317,8 +310,7 @@ useEffect(() => {
             <Text style={styles.third3}>{'Total Payable Amount'}</Text>
             <Text style={[styles.third2]}>₹ {amount}</Text>
           </View>
-    </View>
-
+        </View>
 
         {/* <View style={[styles.cardContainer2]}>
           <View
@@ -353,8 +345,7 @@ useEffect(() => {
               placeholder="Enter Coupon Code"
               placeholderTextColor={'#C9C9C9'}
             />
-            <TouchableOpacity
-              style={styles.buttoncontainer1}>
+            <TouchableOpacity style={styles.buttoncontainer1}>
               <Text style={styles.btext}>{'APPLY'}</Text>
             </TouchableOpacity>
           </View>
@@ -415,7 +406,7 @@ useEffect(() => {
             <Text style={styles.otherIconText}>Cash on Delivery</Text>
 
             <View style={styles.radioBtnContainer}>
-            <RadioButton
+              <RadioButton
                 value="cod"
                 status={radioActive === 'cod' ? 'checked' : 'unchecked'}
                 onPress={() => setRadioActive('cod')}
@@ -434,7 +425,7 @@ useEffect(() => {
             <Text style={styles.otherIconText}>UPI</Text>
 
             <View style={styles.radioBtnContainer}>
-            <RadioButton
+              <RadioButton
                 value="upi"
                 status={radioActive === 'upi' ? 'checked' : 'unchecked'}
                 onPress={() => setRadioActive('upi')}
@@ -453,7 +444,7 @@ useEffect(() => {
             <Text style={styles.otherIconText}>Credit/Debit Card</Text>
 
             <View style={styles.radioBtnContainer}>
-            <RadioButton
+              <RadioButton
                 value="card"
                 status={radioActive === 'card' ? 'checked' : 'unchecked'}
                 onPress={() => setRadioActive('card')}
@@ -472,7 +463,7 @@ useEffect(() => {
             <Text style={styles.otherIconText}>Net Banking</Text>
 
             <View style={styles.radioBtnContainer}>
-            <RadioButton
+              <RadioButton
                 value="net banking"
                 status={radioActive === 'net banking' ? 'checked' : 'unchecked'}
                 onPress={() => setRadioActive('net banking')}
@@ -492,54 +483,49 @@ useEffect(() => {
           />
         </Text>
 
-
         <TouchableOpacity
-      onPress={() => {
-        if (radioActive) {
-          createbyord();
-          // navigation.navigate('Succes');
-        }
-      }}
-      disabled={!radioActive} // Disable the button if COD is not active
-      style={[
-        styles.book,
-        {
-          backgroundColor: radioActive
-            ? nav?.data1 === 'Remedies'
-              ? colors.orange
-              : colors.lightGrey
-            : nav?.data1 === 'Remedies'
-            ? colors.lightGrey
-            : colors.orange, // Opposite color logic
-          shadowColor: radioActive
-            ? nav?.data1 === 'Remedies'
-              ? '#ad3803'
-              : 'black'
-            : nav?.data1 === 'Remedies'
-            ? 'black'
-            : '#ad3803', // Opposite shadow color
-        },
-      ]}
-    >
-      <Text
-        style={[
-          styles.btext1,
-          // {
-          //   color: radioActive
-          //     ? nav?.data1 === 'Remedies'
-          //       ? 'white'
-          //       : 'black'
-          //     : nav?.data1 === 'Remedies'
-          //     ? 'black'
-          //     : 'white', // Opposite text color
-          // },
-        ]}
-      >
-        PROCEED TO PAY
-      </Text>
-    </TouchableOpacity>
-
-
+          onPress={() => {
+            if (radioActive) {
+              createbyord();
+              // navigation.navigate('Succes');
+            }
+          }}
+          disabled={!radioActive} // Disable the button if COD is not active
+          style={[
+            styles.book,
+            {
+              backgroundColor: radioActive
+                ? nav?.data1 === 'Remedies'
+                  ? colors.orange
+                  : colors.lightGrey
+                : nav?.data1 === 'Remedies'
+                ? colors.lightGrey
+                : colors.orange, // Opposite color logic
+              shadowColor: radioActive
+                ? nav?.data1 === 'Remedies'
+                  ? '#ad3803'
+                  : 'black'
+                : nav?.data1 === 'Remedies'
+                ? 'black'
+                : '#ad3803', // Opposite shadow color
+            },
+          ]}>
+          <Text
+            style={[
+              styles.btext1,
+              // {
+              //   color: radioActive
+              //     ? nav?.data1 === 'Remedies'
+              //       ? 'white'
+              //       : 'black'
+              //     : nav?.data1 === 'Remedies'
+              //     ? 'black'
+              //     : 'white', // Opposite text color
+              // },
+            ]}>
+            PROCEED TO PAY
+          </Text>
+        </TouchableOpacity>
 
         {/* <TouchableOpacity
           onPress={() =>createbyord()
@@ -557,7 +543,6 @@ useEffect(() => {
           <Text style={styles.btext1}>PROCEED TO PAY</Text>
         </TouchableOpacity> */}
       </ScrollView>
-  
     </View>
   );
 };
